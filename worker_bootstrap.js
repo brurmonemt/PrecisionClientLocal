fe366d381bb5f37147ffd7134e861e0f25678a69
brur <84108709+brurmonemt@users.noreply.github.com>
2023-11-29 20:27:58 -0600

    Update worker_bootstrap.js

--- worker_bootstrap.js
+++ worker_bootstrap.js
@@ -1,3 +1,6 @@
+// Allow access to this file if using html.cafe
+Access-Control-Allow-Origin: https://html.cafe
+
 onmessage = function (o) {
     importScripts("https://raw.githubusercontent.com/brurmonemt/PrecisionClientLocal/main/classes_server.js");
     eaglercraftServerOpts = o.data;
